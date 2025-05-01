b332bb482f2d65322948a17be60dfded4e04c36a
Kevin Hoxha <K.Hoxha@student.tudelft.nl>
2025-05-01 21:49:51 +0200

    Add ENV variable for HOST and PORT

--- model_service/service.py
+++ model_service/service.py
@@ -1,11 +1,16 @@
 from flask import Flask, request, jsonify
 from model_service.model_utils import load_model
+import os
 # from lib_ml.preprocessing import preprocess_text
 
 app = Flask(__name__)
 
 # Load the model when the application starts
 model = load_model()
+
+# Get host and port from environment variables or use defaults
+HOST = os.environ.get("MODEL_SERVICE_HOST", "0.0.0.0")
+PORT = int(os.environ.get("MODEL_SERVICE_PORT", 5000))
 
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -26,3 +31,6 @@ def predict():
     prediction = model.predict([text])
     
     return jsonify({"text": text, 'prediction': prediction})
+
+if __name__ == "__main__":
+    app.run(host=HOST, port=PORT)
