4e6abac3b4e73c6846636abec001b9210dd7b09e
Kevin Hoxha <K.Hoxha@student.tudelft.nl>
2025-05-04 14:24:54 +0200

    Adapt service.py to match lib_ml

--- model_service/service.py
+++ model_service/service.py
@@ -2,14 +2,16 @@ from flask import Flask, request, jsonify
 from model_utils import load_model
 from flasgger import Swagger, swag_from
 import os
-# from lib_ml.preprocessing import preprocess_text
-# from lib_ml import __version__ as lib_ml_version
+import pandas as pd
+
+from lib_ml import preprocessing
+from lib_ml import __version__ as lib_ml_version
 
 app = Flask(__name__)
 swagger = Swagger(app)
 
 # Load the model when the application starts
-# model = load_model()
+cv, model = load_model()
 
 # Get host and port from environment variables or use defaults
 HOST = os.environ.get("MODEL_SERVICE_HOST", "0.0.0.0")
@@ -27,12 +29,14 @@ def predict():
         return jsonify({'error': 'Invalid input'}), 400
     
     text = data['text']
-    
-    # TODO: Preprocess the text
-    # processed_text = preprocess_text(text)
+
+    # Convert text to the format required by the model (pandas DataFrame with a single column 'Review')
+    text_df = pd.DataFrame([text], columns=['Review'])
+    corpus = preprocessing(text_df)
+    transformed_corpus = cv.transform(corpus)
     
     # Make prediction
-    sentiment = True if model.predict([text])[0] == 1 else False
+    sentiment = True if model.predict(transformed_corpus)[0] == 1 else False
     
     return jsonify({"text": text, 'sentiment': sentiment})
 
@@ -43,11 +47,7 @@ def version():
     """
     Endpoint to get the version of the model from lib_ml.
     """
-    # For now, we will return a hardcoded version
-    return jsonify({"version": "1.0.0"})
-
-    # TODO: Uncomment the following lines when lib_ml is available
-    # return jsonify({"version": lib_ml_version})
+    return jsonify({"version": lib_ml_version})
     
 
 if __name__ == "__main__":
